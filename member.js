9e8cf2b13adadb0f0147dc01607a6479bb22eea6
Kavantheran <63284570+Kavantheran@users.noreply.github.com>
2023-08-07 11:35:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'];
+    var member = {
+        name: 'John',
+        age: 30,
+        skills: skills
+    };
+    console.log(member.skills[2]);
+}
