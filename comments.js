6fc766767afbe988a4316db44f4e256fb9988934
Kavantheran <63284570+Kavantheran@users.noreply.github.com>
2023-08-07 12:22:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,75 +1,39 @@
-// create web server
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
+// create web server with Node.js
+// run with: node comments.js
+// then visit: http://localhost:3000/comments
 
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
+// load http module
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
 
-// Path: index.js
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
+// create http server
+http.createServer(function (req, res) {
+  // content header
+  res.writeHead(200, {'content-type': 'text/html'});
+  
+  // incase of POST request
+  if (req.method == 'POST') {
+    var body = '';
+    req.on('data', function(data) {
+      body += data;
+    });
+    req.on('end', function() {
+      var POST = qs.parse(body);
+      console.log(POST);
+    });
+  }
+  
+  // incase of GET request
+  if (req.method == 'GET') {
+    var url_parts = url.parse(req.url, true);
+    console.log(url_parts.query);
+  }
 
-// Path: comments.js
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
+  // write body of response
+  res.end('<html><body><h1>Hello, World!</h1></body></html>');
 
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-
-// Path: index.js
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-
-// Path: comments.js
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-
-// Path: index.js
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-
-// Path: comments.js
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-
-// Path: index.js
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-
-// Path: comments.js
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-
-// Path: index.js
+}).listen(3000); // listen for connections on this port
+console.log('Server running at http://
 
