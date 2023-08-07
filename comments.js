a15e4dfcdc08eb3c426ba5651bf80e7fb4bcbaf9
Kavantheran <63284570+Kavantheran@users.noreply.github.com>
2023-08-07 11:39:55 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,75 @@
+// create web server
+// create a route for comments
+// create a route for comments/new
+// create a route for comments/:id
+// create a route for comments/:id/edit
+
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+
+// Path: index.js
+// create a route for comments
+// create a route for comments/new
+// create a route for comments/:id
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+
+// Path: comments.js
+// create a route for comments
+// create a route for comments/new
+// create a route for comments/:id
+// create a route for comments/:id/edit
+
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+
+// Path: index.js
+// create a route for comments
+// create a route for comments/new
+// create a route for comments/:id
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+
+// Path: comments.js
+// create a route for comments
+// create a route for comments/new
+// create a route for comments/:id
+// create a route for comments/:id/edit
+
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+
+// Path: index.js
+// create a route for comments
+// create a route for comments/new
+// create a route for comments/:id
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+
+// Path: comments.js
+// create a route for comments
+// create a route for comments/new
+// create a route for comments/:id
+// create a route for comments/:id/edit
+
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+
+// Path: index.js
+// create a route for comments
+// create a route for comments/new
+// create a route for comments/:id
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+
+// Path: comments.js
+// create a route for comments
+// create a route for comments/new
+// create a route for comments/:id
+// create a route for comments/:id/edit
+
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+
+// Path: index.js
+
